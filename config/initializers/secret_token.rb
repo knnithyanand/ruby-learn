6474fa032a573c8285e7251d64f871357b89593a
knnithyanand <knnithyanand@hotmail.com>
2014-05-17 09:30:22 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyLearn::Application.config.secret_token = '8dba64682ebf3cbb8cd9147ceeff16e9cf7768b8cdc35172dc060b1991e498bd610256a0c6c594e60476e400d035c68fa5a0114e5740d17a6290efd9b5786af0'
