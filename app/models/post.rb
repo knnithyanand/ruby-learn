2440734cab463243f8ec9a24f164416a31269380
knnithyanand <knnithyanand@hotmail.com>
2014-05-21 07:00:57 +0000

    Added Humanize class to convert dates t ohuman friendly statements. Posts use Humanize to format date and views updated accordingly.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,31 +2,14 @@ class Post
   include Mongoid::Document
   include Mongoid::Timestamps
   
-  mount_uploader :avatar, AvatarUploader
-  
   field :title, type: String
   field :description, type: String
   
   def created
-    diff = Integer(Time.now - created_at)
-    if diff <= 86400
-      case diff
-        when 0..5 then "now."
-        when 6..60 then "few seconds ago."
-        when 61..120 then "a minute ago."
-        when 121..3300 then "#{ diff / 60 } minutes ago."
-        when 3301..5400 then "about an hour ago."
-        else "#{ diff / 3600 } hours ago."
-      end
-    else
-      case diff /= 86400
-        when 0..1 then "yesterday."
-        when 2..7 then "#{ diff } days ago."
-        when 8..13 then "last week."
-        when 14..30 then "#{ diff / 7 } weeks ago."
-        when 30..60 then "last month."
-        else "#{ diff / 30 } months ago."
-      end
-    end
+    'created ' + Humanize.format_date(created_at)
+  end
+
+  def updated
+    'last updated ' + Humanize.format_date(updated_at)
   end
 end
