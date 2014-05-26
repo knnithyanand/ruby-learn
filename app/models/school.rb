cdfe7c961bbf9e24d5f3e02e37d596295a5ef2c4
knnithyanand <knnithyanand@hotmail.com>
2014-05-26 02:41:44 +0000

    Added CRUD for schools, updated stylesheets for grouped-lists

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,7 @@
 class School
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :name, type: String
   field :building, type: String
   field :street, type: String
@@ -10,6 +12,21 @@ class School
   field :postcode, type: String
   field :phone, type: String
   field :fax, type: String
+  field :currency, type: String
   field :email, type: String
   field :notes, type: String
+  
+  validates_presence_of :name, :phone, :email
+  validates_uniqueness_of :name, :phone, :email, :case_sensitive => false
+
+  ## Image
+  mount_uploader :image, ImageUploader
+  
+  def created
+    Humanize.format_date('created ', created_at)
+  end
+
+  def updated
+    Humanize.format_date('last updated ', updated_at)
+  end
 end
